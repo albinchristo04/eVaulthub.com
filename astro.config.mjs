8c1bc6c6fd87390ad74358989cfeff50bb273d6f
albinchristo04 <priyanka0101111@gmail.com>
2026-01-31 17:57:07 +0530

    v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,25 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://dx.evaulthub.com',
   base: '/',
   output: 'static',
+  adapter: vercel({
+    output: 'static'
+  }),
   build: {
     format: 'directory'
   },
   vite: {
     build: {
       cssMinify: true,
-      minify: true
+      minify: true,
+      rollupOptions: {
+        output: {
+          manualChunks: undefined
+        }
+      }
     }
   }
 });
